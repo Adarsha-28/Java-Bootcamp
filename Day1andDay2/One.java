1d8b7d79316b9c3627ffd948ad98899dde5894d4
Adarsha-28 <adarsha.u2024cse@sece.ac.in>
2025-07-10 21:19:38 +0530

    Bootcamp Day1 and Day2

---
+++ Day1andDay2/One.java
@@ -0,0 +1,7 @@
+public class One {
+    public static void main(String args[]) {
+        System.out.println("Hai from One!");
+        System.out.println("String "+args[0]);
+        System.out.println("Integer "+Integer.parseInt(args[1]));
+    }
+}
---
+++ Day1andDay2/member-search-index.js
@@ -0,0 +1,1 @@
+memberSearchIndex = [{"p":"<Unnamed>","c":"One","l":"main(String[])","u":"main(java.lang.String[])"},{"p":"<Unnamed>","c":"One","l":"One()","u":"%3Cinit%3E()"}];updateSearchResults();
